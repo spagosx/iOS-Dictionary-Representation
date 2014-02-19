35bf8c7aa8622a3a1a6c0d693d7216b282553459
Daniele Spagnolo <d.spagnolo@me.com>
2014-02-19 21:55:08 +0000

    SOX - Added doxygen complaint header comment.

--- Dict Package Sample/NSObject+SOXDictRepresentation.h
+++ Dict Package Sample/NSObject+SOXDictRepresentation.h
@@ -10,4 +10,16 @@
 
 @interface NSObject (SOXDictRepresentation)
 
+/**
+ *
+ * Get all the properties from the receiving object 
+ * and package a NSDictionary object with a key/value pair
+ * for every property declared in the receiving object interface
+ * where the key is the property name and the value is the property value.
+ *
+ * @return A NSDictionary object with a key/value pair for every property declared in the receiving object class interface.
+ *
+ */
+- (NSDictionary *)dicionaryRepresentation;
+
 @end
