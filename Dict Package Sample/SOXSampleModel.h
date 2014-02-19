815530c0f0282b94404168bc6a60b0dd924d588f
Daniele Spagnolo <d.spagnolo@me.com>
2014-02-19 22:08:09 +0000

    SOX - Added SampleModel object to use to demonstrate category functionality.

---
+++ Dict Package Sample/SOXSampleModel.h
@@ -0,0 +1,29 @@
+//
+//  SOXSampleModel.h
+//  Dict Package Sample
+//
+//  Created by Daniele Spagnolo on 19/02/2014.
+//  Copyright (c) 2014 SpagOSX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//
+//
+// This class is a sample model to show the functionality of the NSObject category
+// For semplicity we assume this class rensamble a Person.
+//
+//
+
+@interface SOXSampleModel : NSObject
+
+@property (copy, nonatomic) NSString *firstName;
+@property (copy, nonatomic) NSString *secondName;
+@property (copy, nonatomic) NSString *surname;
+@property (nonatomic) NSNumber *age;
+@property (nonatomic) NSNumber *height; // will use in feet with decimal value
+@property (nonatomic) NSNumber *weight; // in pounds
+@property (copy, nonatomic) NSString *telephone; // americans don't use just numbers?
+@property (nonatomic) NSString *address;
+
+@end
---
+++ Dict Package Sample/SOXSampleModel.m
@@ -0,0 +1,32 @@
+//
+//  SOXSampleModel.m
+//  Dict Package Sample
+//
+//  Created by Daniele Spagnolo on 19/02/2014.
+//  Copyright (c) 2014 SpagOSX. All rights reserved.
+//
+
+#import "SOXSampleModel.h"
+
+@implementation SOXSampleModel
+
++ (id)samplePerson {
+    return [[self alloc] init];
+}
+
+- (id)init {
+    if (self = [super init]) {
+        _firstName = @"John";
+        _secondName = @"Arthur";
+        _surname = @"Smith";
+        _age = @42;
+        _height = @5.80;
+        _weight = @165;
+        _telephone = @"+44 123 456 789";
+        _address = @"1 The Street, Anytown, Nicecounty, XY3 ZA2, UK";
+    }
+    
+    return self;
+}
+
+@end
